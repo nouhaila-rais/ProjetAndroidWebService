b996c950ce3211cd9d00fca612ca43faa4bb8617
Mouna Serrar <mounaserrar21@gmail.com>
2022-03-18 10:24:10 +0100

    Modifier Spring Security

--- src/main/java/fr/ugesellsloaning/api/controllers/UserController.java
+++ src/main/java/fr/ugesellsloaning/api/controllers/UserController.java
@@ -6,6 +6,7 @@ import fr.ugesellsloaning.api.services.AccountServices;
 import fr.ugesellsloaning.api.services.UserServices;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,6 +35,8 @@ public class UserController {
 
     Principal principal;
 
+    UserDetails userDetails;
+
     @GetMapping(path = "/user/")
     public List<User> list(){
         return (List<User>) userServices.listUser();
@@ -41,7 +44,7 @@ public class UserController {
 
     @PostMapping(path = "/register")
     public void register(@Valid @RequestBody User user){
-        //user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userServices.save(user);
         if(user.getRole().equals("Customer")){
             Account account = new Account();
@@ -64,6 +67,41 @@ public class UserController {
             else return -1;
         }
         return -2;
+    }
+
+    @PostMapping("/secured/test")
+    public int logintest(@RequestBody User user){
+        User user1 = userServices.getUserByEmail(user.getEmail());
+
+        if(user1 != null){
+            //String password = passwordEncoder.encode(user.getPassword());
+            //System.out.println(password);
+            System.out.println(user1.getPassword());
+            //User currentUser = (User)request.getAttribute("userName");
+
+            if(user.getEmail().equals(user1.getEmail()) && user.getPassword().equals(user1.getPassword())) {
+                System.out.println("user current " + principal.getName());
+                return (int) user1.getId();
+            }
+            else return -1;
+        }
+        return -2;
+    }
+
+    // @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping("/secured/all")
+    public String securedHello() {
+
+        principal = request.getUserPrincipal();
+        System.out.println("Secured Hello" + principal.getName());
+        return principal.getName();
+    }
+
+    @GetMapping("/secured/test/")
+    public String securedHell() {
+        principal = request.getUserPrincipal();
+        System.out.println("Secured Hello" + principal.getName());
+        return principal.getName();
     }
 
     @GetMapping(path = "/user/{id}")
---
+++ src/main/java/fr/ugesellsloaning/api/security/WebSecurity.java
@@ -0,0 +1,63 @@
+package fr.ugesellsloaning.api.security;
+
+//import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import fr.ugesellsloaning.api.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private static final String DEFAULT_INCLUDE_PATTERN = "/api/toRemove/**";
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    protected  void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                //--------Swagger authorisation------------
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/webjars/springfox-swagger-ui/**").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/favicon.ico").permitAll()
+                .antMatchers("**/*.html").permitAll()
+                .antMatchers("**/*.css").permitAll()
+                .antMatchers("**/*.js").permitAll()
+                //----------------------------------------
+                .anyRequest().permitAll()
+                //.anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authBuilder) throws Exception{
+        authBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration(DEFAULT_INCLUDE_PATTERN, new CorsConfiguration().applyPermitDefaultValues());
+
+        return source;
+    }
+}
